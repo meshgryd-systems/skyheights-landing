61577e1c76e57e12a5cf338ddf8b24393acec28c
Giwa Muhammad <mgiwa78@gmail.com>
2026-01-26 20:50:31 +0100

    Refactor ContactForm imports to use ContactFormWrapper for consistency; remove unnecessary mounted state logic in ClientProviders for cleaner code.

--- src/providers/ClientProviders.tsx
+++ src/providers/ClientProviders.tsx
@@ -1,20 +1,7 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import StoreProvider from './StoreProvider';
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setMounted(true);
-    }, 100);
-  }, []);
-
-  if (!mounted) {
-    return <>{children}</>;
-  }
-
   return <StoreProvider>{children}</StoreProvider>;
 }
